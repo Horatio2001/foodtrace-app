57789b3973042ad22e222208d6bcd2da4116dd06
杨大智 <19231165@buaa.edu.cn>
2023-03-14 13:05:57 +0800

    init

---
+++ src/main/java/com/example/foodtrace/FoodtraceApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodtrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodtraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodtrace/FoodtraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodtrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
